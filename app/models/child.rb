01b7b5acbbf6e0f723ee234d79a08f0912dfd8c8
Luia Yen <luia.yen@gmail.com>
2016-10-21 16:53:19 -0400

    return active children only

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,11 +1,12 @@
 class Child < ActiveRecord::Base
-  has_many(:chores)
+  has_many :chores
   has_many :tasks, through: :chores
   
   validates_presence_of :first_name
   validates_presence_of :last_name
   
   scope :alphabetical, -> { order('last_name, first_name') }
+  scope :active, -> { where('active is ?', true) }
   
   def name
     "#{first_name} #{last_name}" 
