d4ba4649306b055d648e20084faf0fee34d722f7
Luia Yen <luia.yen@gmail.com>
2016-10-21 16:38:33 -0400

    scope test

--- app/models/child.rb
+++ app/models/child.rb
@@ -5,6 +5,8 @@ class Child < ActiveRecord::Base
   validates_presence_of :first_name
   validates_presence_of :last_name
   
+  scope :alphabetical, -> [ order('first_name, last_name')]
+  
   def name
     "#{first_name} #{last_name}" 
   end
