99d89c17d75cdb876ec9047d53495c01e26a1906
Luia Yen <luia.yen@gmail.com>
2016-10-21 16:40:53 -0400

    fixed alphabetical scope

--- app/models/child.rb
+++ app/models/child.rb
@@ -5,7 +5,7 @@ class Child < ActiveRecord::Base
   validates_presence_of :first_name
   validates_presence_of :last_name
   
-  scope :alphabetical, -> [ order('first_name, last_name')]
+  scope :alphabetical, -> { order('last_name, first_name') }
   
   def name
     "#{first_name} #{last_name}" 
